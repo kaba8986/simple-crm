90f9bc7a65d6c6cff9df9d31fc2060dcae64653e
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-01-23 10:09:36 +0100

    show users by loading from firestore database, add userID from firebase into user-model

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-user',
@@ -11,10 +12,15 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 export class UserComponent implements OnInit {
 
   user = new User();   
+  allUsers = [];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.firestore.collection('users').valueChanges({idField: 'customIdName'}).subscribe((changes:any) => {
+      console.log('Recieved changes from database: ', changes);
+      this.allUsers = changes;
+    })
   }
 
   openDialog() {
