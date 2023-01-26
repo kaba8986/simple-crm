8c20b6deff3763b0e76e9a5f164afe1ce9a075fd
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-01-26 11:56:07 +0100

    sort firebase collection by loading

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Contact } from 'src/models/contact.class';
 import { DialogAddContactComponent } from '../dialog-add-contact/dialog-add-contact.component';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
@@ -11,37 +11,26 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class ContactsComponent implements OnInit {
 
-  contact = new Contact();   
-  allContacts = [];
+  contact = new Contact();
+  allContacts: any = [];
+  sortedContacts: any = [];
 
   constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+
     this.firestore
-    .collection('contacts')
-    .valueChanges({idField: 'customIdName'})
-    .subscribe((changes:any) => {
-      this.allContacts = changes;
-    })
-    //Sort User-Array alphabetically by lastname
-    this.allContacts.sort((a:any, b:any) => {
-      return this.compareStrings(a.lastName, b.lastName)
-    })
-    
+      .collection('contacts', ref => ref.orderBy('lastName')) //Sort User-Array alphabetically by lastname
+      .valueChanges({ idField: 'customIdName' })
+      .subscribe((changes: any) => {
+        this.allContacts = changes;
+      })
+
   }
 
   openDialog() {
     this.dialog.open(DialogAddContactComponent);
   }
-
-  compareStrings(a, b) {
-    // Assuming you want case-insensitive comparison
-    a = a.toLowerCase();
-    b = b.toLowerCase();
-  
-    return (a < b) ? -1 : (a > b) ? 1 : 0;
-  }
-
 
 
 }
