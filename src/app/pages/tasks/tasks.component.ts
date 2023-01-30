51ff9615115b4838d5374567af22492747e29ed2
Andreas Burghardt <andreas-burghardt@gmx.net>
2023-01-30 10:38:14 +0100

    load tasks from firestore and display in right category-columns

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddTaskComponent } from 'src/app/components/dialog-add-task/dialog-add-task.component';
 
@@ -9,11 +10,21 @@ import { DialogAddTaskComponent } from 'src/app/components/dialog-add-task/dialo
 })
 export class TasksComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
-  colHeads = ['To do', 'In Progress', 'Awaiting Feedback', 'Done'];
+  colHeads = [
+    {value: 0, name: 'To do' },
+    {value: 1, name: 'In Progress' },
+    {value: 2, name: 'Awaiting Feedback' },
+    {value: 3, name: 'Done' }
+  ];
+
+  allTasks: any = [];
 
   ngOnInit(): void {
+    this.firestore.collection('tasks').valueChanges().subscribe((data: any) => {
+      this.allTasks = data;
+    })
   }
 
   openDialog(sub: number) {
